ab878fb081f4a4ad4e004692f26a2efe3240b309
UnLegit <ZubMax2005@yandex.ru>
2024-05-15 15:44:00 +0300

    build: modify gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "ru.unlegit"
@@ -14,4 +15,22 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.32")
     annotationProcessor("org.projectlombok:lombok:1.18.32")
     compileOnly("org.spigotmc:spigot:1.12.2-R0.1-SNAPSHOT")
+}
+
+tasks {
+    jar {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        configurations.runtimeClasspath.get().files.forEach { from(zipTree(it)) }
+    }
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            from(components["java"])
+        }
+    }
 }
